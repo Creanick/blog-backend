b06e4ecf76f0e03fd905cf18ebbea756bbb9487a
manick <manickware@gmail.com>
2019-09-24 12:23:43 +0530

    configuration file added for better management

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+const { port, dbUrl, dbName } = require("./config");
 
-const port = process.env.PORT || 5000;
-const dbUrl =
-  "mongodb+srv://guest:MUvoUv4dA5v678SN@cluster0-2hvb3.mongodb.net/blog_backend_db?retryWrites=true&w=majority";
 app.listen(port, () => {
   console.log("server running at %d", port);
   mongoose
     .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-      console.log("Database Successfully connected");
+      console.log(`Database: ${dbName} Successfully connected`);
     })
     .catch(err => {
       console.log("Database Connection Error: ", err.message);
