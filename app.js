a61599b6f4c304f98c88117c251a9a53dee8747d
manick <manickware@gmail.com>
2019-09-24 15:30:54 +0530

    add json body parser support

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const postRouter = require("./routes/post");
 const BlogError = require("./utils/BlogError");
 const app = express();
+app.use(bodyParser.json());
 app.use(postRouter);
 app.use((req, res, next) => {
   res.status(404).send("api not found");
