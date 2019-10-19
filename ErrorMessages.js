36e76f28a03f787be3fb98040d1229e4ef60b939
creanick <manickware@gmail.com>
2019-10-19 10:21:28 +0530

    add limit and skip error messages

--- ErrorMessages.js
+++ ErrorMessages.js
@@ -4,5 +4,7 @@ module.exports = {
   invalidId: "Post id is invalid",
   postNotFound: "Post not found",
   title: "Title length must be 5 or more long",
-  content: "Content length must be 5 or more long"
+  content: "Content length must be 5 or more long",
+  limit: "limit should be a positive numeric value",
+  skip: "skip should be a positive numeric value"
 };
