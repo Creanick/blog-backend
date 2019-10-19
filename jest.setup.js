55100170fb39c0d52fc177b5506a8a93b560b77f
creanick <manickware@gmail.com>
2019-10-19 09:07:38 +0530

    clear mongoose post collection before each test

--- jest.setup.js
+++ jest.setup.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
-
+const Post = require("./models/post");
 beforeAll(async () => {
   await mongoose.connect(global.__MONGO_URI__, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
 });
-
+beforeEach(async () => {
+  await Post.deleteMany({});
+});
 afterAll(() => {
   mongoose.connection.close();
 });
