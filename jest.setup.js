161ce1f8cc0cc85db02841f044ebd3f6a4041f9e
creanick <manickware@gmail.com>
2019-10-19 10:58:33 +0530

    add post deletemany on after all in jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const Post = require("./models/post");
 beforeAll(async () => {
   await mongoose.connect(global.__MONGO_URI__, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
 });
-afterAll(() => {
+afterAll(async () => {
+  await Post.deleteMany({});
   mongoose.connection.close();
 });
